03c65642e45249ecf3a856c512eaec4c15d10e47
dianavashti <diana.vashti@gmail.com>
2017-11-01 14:30:14 -0700

    Hides solutions in folder

--- mySolutions/indexRouteSolution.js
+++ mySolutions/indexRouteSolution.js
@@ -4,15 +4,24 @@ const {Querries} = require('../database/db.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index')
+  Querries.getAll()
+  .then( todoz => {
+    res.render('index', {todoz})
+  })
 });
 
 router.post('/add', function(req, res, next) {
-  // add your code here
+  Querries.add(req.body.item)
+  .then( () => {
+    res.redirect('/')
+  })
 })
 
 router.get('/complete/:id', function(req, res, next){
-  // add your code here
+  Querries.complete(req.params.id)
+  .then( () => {
+    res.redirect('/')
+  })
 })
 
 module.exports = router;
---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO todoz (item) VALUES ('First Item');
+
+INSERT INTO todoz (item) VALUES ('Second Item');
+
+INSERT INTO todoz (item) VALUES ('Third Item');
