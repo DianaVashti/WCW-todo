65b1326e4479d81918ac3f0c98bb47037234f921
dianavashti <diana.vashti@gmail.com>
2017-11-01 14:15:33 -0700

    Finishes basic todo list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const {Querries} = require('../database/db.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Querries.getAll()
+  .then( todoz => {
+    res.render('index', {todoz})
+  })
 });
+
+router.post('/add', function(req, res, next) {
+  Querries.add(req.body.item)
+  .then( () => {
+    res.redirect('/')
+  })
+})
+
+router.get('/complete/:id', function(req, res, next){
+  Querries.complete(req.params.id)
+  .then( () => {
+    res.redirect('/')
+  })
+})
 
 module.exports = router;
