6bc1046cb4ae07a0c783891d21f938a78cf3bd7f
dianavashti <diana.vashti@gmail.com>
2017-12-27 18:30:49 -0500

    adds a lot...

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes')
 
+const ROOT_DIR = path.resolve(__dirname, '../')
+
 var app = express();
 
 // view engine setup
@@ -19,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(`${ROOT_DIR}/public`))
 
 app.use(routes);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 
-var users = require('./users')
+import users from './users'
+import auth from './auth'
 
 var router = express.Router();
-const {Querries} = require('../database/db.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // write auth funx and only go home if no session active-user
   res.render('partials/home')
 });
+
+router.use('/auth', auth)
 
 module.exports = router;
